2f424b442758f778162f162cfcca0d33d85ba1d0
Valentina <dysfnalv@gmail.com>
2025-01-13 20:20:17 -0600

    WIP: get endpoint

---
+++ top-bod-be/Controllers/NutritionGet.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+//using top_bod_be.Models;
+
+namespace top_bod_be.Controllers
+{
+    [ApiController]
+    //TODO: should I update the route? Do I need to?
+    [Route("api/[controller]")]
+    public class NutritionGet : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpFactory;
+
+        public NutritionGet(IHttpClientFactory httpFactory)
+        {
+            _httpFactory = httpFactory;
+        }
+
+        //[HttpGet]
+        //public async Task<List<NutritionDetails>> GetNutritionDetails(string query)
+        //{
+        //    var client = _httpFactory.CreateClient();
+        //    //TODO: build URL
+        //    var response = await client.GetAsync(query);
+        //}
+    }
+}
