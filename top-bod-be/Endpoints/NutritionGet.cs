45a463e15e0bc04ed74d044c92c199158243c3a3
Valentina <dysfnalv@gmail.com>
2025-01-16 21:36:17 -0600

    Changed endpoint folder name and added endpoint logic

---
+++ top-bod-be/Endpoints/NutritionGet.cs
@@ -0,0 +1,35 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+using top_bod_be.Models;
+using top_bod_be.Services;
+
+namespace top_bod_be.Controllers
+{
+    public class NutritionGet : EndpointBaseAsync
+        .WithRequest<string>
+        .WithResult<List<NutritionDetails>>
+    {
+        private readonly INutritionService _nutritionService;
+
+        public NutritionGet(INutritionService nutritionService)
+        { 
+            _nutritionService = nutritionService;
+        }
+
+        [HttpGet("api/nutrition")]
+        public override Task<List<NutritionDetails>> HandleAsync(
+            [FromQuery(Name ="query")] string request, 
+            CancellationToken cancellationToken = default)
+        {
+            if (request == null || request == string.Empty)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            //TODO: need to add error handling service
+            var queryNutrition = _nutritionService.SearchReturnsNutritionDetails(request);
+
+            return queryNutrition;
+        }
+    }
+}
